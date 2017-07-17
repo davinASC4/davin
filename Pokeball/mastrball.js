2f0c1a28c5b36fe224bb0fef7f3d3e361f77fffa
davinASC4 <davlin429@yahoo.com>
2017-07-17 13:49:25 -0700

    New stuff

---
+++ Pokeball/mastrball.js
@@ -0,0 +1,23 @@
+
+
+function setup(){
+    createCanvas(300,300);
+    background(205);     
+    stroke("black");
+    strokeWeight(6);    
+    arc (width/2, height/2, 200,200, 0, PI, CHORD);
+
+    fill("purple");
+    arc (width/2, height/2, 200, 200, PI, 0, CHORD)
+
+    fill("white");
+    ellipse(width/2, height/2, 50, 50);
+
+    fill("black");
+    ellipse(width/2, height/2, 25, 25);
+
+    textSize(40);
+    text("M", 135, 100);
+}
+
+
