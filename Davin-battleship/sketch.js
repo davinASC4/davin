540392f33d3e5135c0faa871736d2876b8f641e3
davinASC4 <davlin429@yahoo.com>
2017-07-19 21:12:41 -0700

    upped

--- Davin-battleship/sketch.js
+++ Davin-battleship/sketch.js
@@ -1,7 +1,7 @@
 
 
 function setup(){
-    createCanvas(500,700);
+    createCanvas(800,700);
     background("Black");
 
 
@@ -65,28 +65,77 @@ for (sh = 0; sh < ship; sh++){
     randomRow = Math.round(Math.random()* 9);
 }
 
-var xloc = 25;
-var yloc = 25;
+var pxloc = 25;
+var pyloc = 25;
 var lsize = 30;
+var cxloc = 25;
+var cyloc = 380;
+ 
 function draw(){
-    
-    fill("White");
-    var p2html = [];
+   
+
+    fill("White");  //generating squares/rects 
+    var phtml = []; //coil = row, coil2 = column
     for (coil = 0; coil < 10; coil ++){
         for (coil2 = 0; coil2 < 10; coil2++){
-            p2html.push(rect(xloc + lsize*(coil2), yloc + lsize* (coil ), lsize, lsize));
+            phtml.push(rect(pxloc + lsize*(coil), pyloc + lsize* (coil2 ), lsize, lsize));
         }
     }
 
-    for (a = 0; a < 10; a++){
-        for (b = 0; b < 10; b++){
+    for (a = 0; a < 10; a++){ //generating text which will have the same centers as certain squares 
+        for (b = 0; b < 10; b++){ // a = row b = column
             if (playgrid[a][b] == true){
                 fill("Orange");
-                text("Ship", xloc + lsize * (b) , yloc + lsize *(a) + lsize/2);
+                text("Ship", pxloc + lsize * (b) , pyloc + lsize *(a) + lsize/2);
             }
         }
     }
     
+
+    //fill("White");  //generating squares/rects 
+    var chtml = []; //coil = row, coil2 = column
+    //for (coil = 0; coil < 10; coil ++){
+        //for (coil2 = 0; coil2 < 10; coil2++){
+            chtml.push(rect(cxloc + lsize*(coil), cyloc + lsize* (coil2 ), lsize, lsize));
+        //}
+    //}
+
+    //for (a = 0; a < 10; a++){ //generating text which will have the same centers as certain squares 
+        //for (b = 0; b < 10; b++){ // a = row b = column
+            //if (compgrid[a][b] == true){
+                fill("Orange");
+                text("Ship", cxloc + lsize * (b) , cyloc + lsize *(a) + lsize/2);
+            //}
+        //}
+    //}
+
+
+
 }
+
+var word = "";
+function keyPressed(){
+    
+    
+    if (keyCode === BACKSPACE){
+    
+        word = word.slice(0, -1);
+        clear();
+         createCanvas(800, 700);
+        background("Black");
+    
+    }
+    else {
+    word = word + key;
+
+    }
+
+    print(word);
+    fill("Orange");
+    text(word, 500, 500);
+
+    return false
+}
+
 
 
