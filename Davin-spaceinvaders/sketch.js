4e14ca8746ddb3c22803ca6a9d49f2488328e572
davinASC4 <davlin429@yahoo.com>
2017-07-21 15:45:07 -0700

    invasion

--- Davin-spaceinvaders/sketch.js
+++ Davin-spaceinvaders/sketch.js
@@ -1,14 +1,17 @@
-// function enemy(color, point, rowz){
+function enemy(color, point, rowz){
 
-//     this.point = point;
-//     this.color = color;
-//     this.rowz = rowz
-// }
+    this.point = point;
+    this.color = color;
+    this.rowz = rowz
+}
 
-// function missile (xp, yp){
-//     this.xp = xp;
-//     this.yp = yp;
-// }
+function Missile (xp, yp, width, height, boolean){
+    this.xp = xp;
+    this.yp = yp;
+    this.boolean = boolean;
+    this.width = width;
+    this.height = height;
+}
 
 
 // var square = new enemy ("red", 25, 0);
@@ -20,44 +23,75 @@ function setup(){
 
 var row = prompt("How many rows would you like? ");
 var column = prompt("How many columns would you like? ");
- 
  var playgrid = [];
  
-var rowd = []
 
-
-for (jam = 0; jam < column; jam++){    //these 'for' loops would make a boolean matrix (?) named playgrid like above;
-     rowd.push(true);
-     }
 for (plug = 0; plug < row; plug++){
-    playgrid.push(rowd);
+                                                     //these 'for' loops would make a boolean matrix (?) named playgrid like above;
+     playgrid.push([])
+     
+    for (jam = 0; jam < column; jam++){  
+        playgrid[plug].push (true);
+    }
+
 }
 
 
+var bullet = new Missile (400, 25, 5, 15, false);
+var bulletmov = 1;
+var enbullet = new Missile (40, 40, 5, 5, false);
+var shoot = Math.round(Math.random());
 var a = 25; //x coordinate of first square
 var b = 25; //y coodrinate of first square
 var c = 1;  //x increment
 var d = 1; //y increment (decrement?)
 var s = 10; //size of squares
-
+var xCoord = 400 //
+var yCoord = 400//
+var shheight = 15;
+var shwidth = 40;
+var life = 3;
 function draw(){
+
+
+    shoot = Math.round(Math.random());
    createCanvas(800,800);
     background("Black");
     fill("White");
-    rect(bullet.xp, bullet.yp, s, s);
-        
-    var phtml = []; //this will make the squares
+    rect(xCoord, yCoord, shwidth, shheight);
+    if (keyIsDown(LEFT_ARROW)) {
+            if (xCoord > 0) {
+                xCoord -= 5;
+            }
+        }
+
+    if (keyIsDown(RIGHT_ARROW)) {
+            if (xCoord + 50 < width) {
+                xCoord += 5;
+            }
+        }
+
+
     fill("red");
+  
     for (ro = 0; ro < playgrid.length; ro ++){                
         for (colum = 0; colum < playgrid[0].length; colum++){
-
-            if (bullet.xp <= a + 2*s*(colum) + s && bullet.xp >= a + 2*s*s*(colum) && bullet.yp <= b + 2 *s* (ro) + s && bullet.yp <= b + 2 *s* (ro)){
-                playgrid[ro][colum] = false;
-
-            }
+            
             if (playgrid[ro][colum] == true){  
-                                        
-                phtml.push(rect(a + 2*s*(colum), b + 2 *s* (ro), s, s));
+                  if (shoot == 1 && enbullet.boolean == false){
+                    enbullet.boolean = true;
+                    enbullet.xp = a + 2*s*(colum)
+                    enbullet.yp = b + 2 *s* (ro)
+                }
+                
+                if ( bullet.xp == a + 2*s*(colum) && bullet.yp ==  b + 2 *s* (ro) && bullet.boolean == true){
+                    bullet.boolean= false;
+                    playgrid[ro][colum] = false;
+                    
+                }
+                else{                
+                rect(a + 2*s*(colum), b + 2 *s* (ro), s, s);
+                }
 
             }
         }
@@ -72,11 +106,59 @@ function draw(){
         b = b + d;
         c = 1;
     }
-    a = a + c              
+    a = a + c      
+    textSize(30);
+    fill("Yellow");
+    text(String(life), 600, 50);
+
+    if (bullet.boolean == false){
+        rect(xCoord + shwidth/2 , yCoord + shheight/2 , bullet.width, bullet.height);
+        
+    }
+
     
+
+    if (bullet.boolean == true){
+
+    }
+
+    if (enbullet.boolean == true){
+        ellipse(enbullet.xp, enbullet.yp, enbullet.width, enbullet.height);
+        enbullet.yp = enbullet.yp + bulletmov;
+
+    }        
+
+    if (enbullet.yp == 500){
+        enbullet.boolean = false;
+    }
+    
+    if (enbullet.yp >= yCoord && enbullet.yp <= yCoord + shheight && enbullet.xp >= xCoord && enbullet.xp <= xCoord + shwidth){
+        enbullet.boolean = false;
+        life = life - 1;
+
+        if (life <= 0){
+            life = life - 1;
+            alert("Game");
+        }
+    }
 
     
 }
+
+function keyPressed(){
+       if (keyIsDown(LEFT_ARROW)) {
+            if (xCoord > 0) {
+                xCoord -= 5;
+            }
+        }
+
+    if (keyIsDown(RIGHT_ARROW)) {
+            if (xCoord + 50 < width) {
+                xCoord += 5;
+            }
+        }
+}
+
 
 //adding later
 //a checking array to compare the x,y positions of the enemies and the moving bullet
--- Test/test.js
+++ Test/test.js
@@ -1,79 +1,19 @@
-//
 
-function enemy(xpos, ypos, color,size){
-    this.xpos = xpos;
-    this.ypos = ypos;
-    this.color = color;
-    this.size = size
+playgrid = [];
+
+for (plug = 0; plug < 3; plug++){
+       //these 'for' loops would make a boolean matrix (?) named playgrid like above;
+     playgrid.push([])
+     
+    for (jam = 0; jam < 4; jam++){  
+        playgrid[plug].push (true);
+    }
+
 }
 
-var square = {
-    xpos: 25,
-    ypos: 25,
-    color: "Red",
-    size: 10
-}
 
-var playgrid = [];
+playgrid[1][1]= false;
+console.log(playgrid);
 
-// for (i = 0; i < 10; i++){
-//     for (j = 0; j < 10; j++){
-//         square.xpos = square.xpos + 20;
-//         square.ypos = square.ypos + 20;
-//         playgrid.push(square);
-
-
-//     }
-// }
-
-// playgrid[0].xpos = 0;
-
-// console.log(playgrid);
-
-// var row = 10;
-// var column = 10;
-// var rowd = [];
-
-// for (plug = 0; plug < row; plug++){   //these 'for' loops would make a boolean matrix (?) named playgrid like above;
-//     for (jam = 0; jam < column; jam++){
-//         rowd.push(true);
-//     }
-
-//     playgrid.push(rowd);
-//     rowd = [];
-//         console.log(rowd);
-
-// }
-
-
-// console.log(playgrid);
-
-
-// for (jam = 0; jam < column; jam++){
-//          rowd.push(true);
-//      }
-// for (plug = 0; plug < row; plug++){
-//     playgrid.push(rowd);
-// }
-
-// rowd =[];
-// console.log(playgrid);
-
-// var te = 0;
-
-// for (i = 0; i < 9; i++){
-//     rowd.push(te);
-//     te = te + 1;
-// }
-
-// console.log(rowd);
-
-// var grid = [];
-// for (i = 0; i < 10; i++){
-//     playgrid.push(grid)
-
-// }
-// grid = [1];
-
-// console.log(playgrid);
+console.log(playgrid[1][1]);
 
