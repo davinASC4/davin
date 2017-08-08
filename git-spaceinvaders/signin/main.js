84f12aa58d5897ea62bdd8e132d18c2daf0c6307
davinASC4 <davlin429@yahoo.com>
2017-08-08 16:00:32 -0700

    update

---
+++ git-spaceinvaders/signin/main.js
@@ -0,0 +1,21 @@
+
+//called when "sign out" button clicked
+function onSignOut() {
+    console.log(gapi.auth2);
+    gapi.auth2.init();
+    //should sign user out and toggleHidden
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.')
+        //setting back to default
+        $(".userName").text("USER_NAME");
+        $("img").attr("src", "assets/placeholder.png");
+        $(".email").text("example@example.com");
+        window.location.href = "../index.html";
+        
+   });
+}
+
+function changepage() {
+    window.location.href = "submit/1.html";
+}
